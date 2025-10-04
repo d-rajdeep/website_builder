a687331a8da454cd56bb9db7175c0577ede29ceb
Rajdeep Dutta <34247617+Rajdeepdutta192@users.noreply.github.com>
2025-10-04 22:55:16 +0530

    footer

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -4,7 +4,7 @@
         <div class="row gy-4">
             <div class="col-lg-4 col-md-6 footer-about">
                 <a href="index.html" class="d-flex align-items-center">
-                    <span class="sitename">eNno</span>
+                    <span class="sitename">Website Name</span>
                 </a>
                 <div class="footer-contact pt-3">
                     <p>A108 Adam Street</p>
